5821958c60957ed823d74668dd517adc29d2d383
sorenmoha <sorenm1997@gmail.com>
2021-12-10 20:40:43 -0500

    first commit for udemy course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
+
   ],
   imports: [
     BrowserModule,
@@ -27,7 +34,9 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     MatButtonModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatSliderModule
+    MatSliderModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+
+@Component ({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+
+})
+
+export class PostListComponent {
+//  posts = [
+//    {title: 'First Post', content: 'This is my first post\'s content'},
+//    {title: 'Second Post', content: 'This is my second post\'s content'},
+//    {title: 'Third Post', content: 'This is my third post\'s content'},
+//  ];
+
+  posts = [];
+
+}

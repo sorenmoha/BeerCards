8546db69d19b2d2e943ca83c94713a23eb4d08ce
sorenmoha <sorenm1997@gmail.com>
2021-12-30 23:18:18 -0500

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing-module';
 
 
 
@@ -30,6 +31,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
@@ -40,7 +42,8 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatSliderModule,
     MatToolbarModule,
     MatExpansionModule,
-    HttpClientModule
+    HttpClientModule,
+  
     
   ],
   providers: [],
